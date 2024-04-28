1eb980f2591b6d3a0d84dea0bcee96b7bc7f3f1c
한상훈 <hzyan2@gmail.com>
2024-04-28 20:46:07 +0900

    폴더 로그 추가

--- src/main/kotlin/lol/cloud/lolcloud/s3/folder/service/FolderServiceImpl.kt
+++ src/main/kotlin/lol/cloud/lolcloud/s3/folder/service/FolderServiceImpl.kt
@@ -1,10 +1,13 @@
 package lol.cloud.lolcloud.s3.folder.service
 
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.io.File
 
 @Service
 class FolderServiceImpl : FolderService {
+
+    private val log = LoggerFactory.getLogger(this.javaClass)
     override fun createFolder(bucketName: String, folderName: String, prefix: String) {
 
         //        val file = File("/home/sang/Desktop/cloud/$filePath")
@@ -14,6 +17,9 @@ class FolderServiceImpl : FolderService {
         val file = File(path)
 
         if(!file.mkdir()) {
+            log.info("폴덩경로: {}", path)
+            log.info("파일: {}", file)
+
             throw IllegalArgumentException("폴더 생성에 실패 했습니다.")
         }
 
